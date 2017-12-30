32db2106c0e799f6c329e1aaaef3703b9c8bd30b
Paul Halliday <hello@paulhalliday.io>
2017-12-30 05:50:49 +0000

    Example of lazy loading routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+Vue.use(VueRouter);
+
+const Hello = () => import('./components/Hello');
+const World = () => import('./components/World');
+
+const routes = [
+  { path: '/', redirect: '/hello' },
+  { path: '/hello', component: Hello },
+  { path: '/World', component: World },
+];
+
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  router,
+  render: h => h(App),
+});
